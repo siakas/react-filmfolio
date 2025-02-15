ef4f12842052e3f531c8b74c297c078db22a903b
Sanshiroh SAKAI <sansyo@gmail.com>
2025-02-15 14:59:30 +0900

    Update: ヘッダのナビゲーションを追加

---
+++ common/components/layout/header/MainNavigationListItem.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { NavigationMenuLink } from "@/common/components/ui/navigation-menu";
+import { cn } from "@/common/lib/utils";
+
+const MainNavigationListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-2 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+MainNavigationListItem.displayName = "MainNavigationListItem";
+
+export { MainNavigationListItem };
